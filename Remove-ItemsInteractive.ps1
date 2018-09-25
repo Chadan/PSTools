eccaa4b2457ff60480381a2a5954651389e6e468
Christian Hernandez Sanchez <chhern@microsoft.com>
2018-09-25 15:36:59 -0700

    Adding Remove-ItemsInteractive

---
+++ Remove-ItemsInteractive.ps1
@@ -0,0 +1,32 @@
+﻿param (
+
+)
+
+Write-Host -ForegroundColor Cyan -Value @"
+This utily deletes any folder or file you copy to it.
+WARNING: This utility will remove the items specified without asking for confirmation, so be careful.
+
+Insert a path per line.
+:Q to quit
+
+"@
+
+$TextLine = (Read-Host).Trim()
+$ErrorMsg = ""
+
+While ($TextLine -ne ":Q" `
+       -and $TextLine -ne ":q") {
+    If ($TextLine.Length -gt 0) {
+        Write-Host "`t$($TextLine): " -ForegroundColor Yellow -NoNewline
+    
+        Remove-Item -Path $TextLine -Force -Recurse -ErrorAction SilentlyContinue
+        If ($? -eq $true) {
+            Write-Host "SUCESS" -ForegroundColor Green
+        }
+        Else {
+            Write-Host "FAIL -> $($Error[0].Exception.Message)" -ForegroundColor Red
+        }
+    }
+
+    $TextLine = (Read-Host).Trim()
+}
